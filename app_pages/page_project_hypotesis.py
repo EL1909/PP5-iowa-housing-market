891a53667961c020930f567ce6d2fdd8496679c6
EL1909 <>
2023-05-10 09:20:05 +0000

    app page hypotesis created

--- app_pages/page_project_hypotesis.py
+++ app_pages/page_project_hypotesis.py
@@ -5,17 +5,14 @@ def page_project_hypothesis_body():
 
     st.success(
         f"### Project Hypothesis and Validation\n\n"
-        f"* I assume an evaluation of sales prices of other houses, "
-        f"in the area of Ames, Iowa, based on attributes similar to attributes"
-        f" of each of the clients 4 inherited houses should provide a "
-        f"prediction of sales price for each house respectively\n\n"
+        f"* We can predict the sales price for any house in Ames iowa. "
+        f"By analysing the database records of the real state"
+        f" transactions made in Ames, state of Iowa, "
+        f"we can predict a sales price for a particular house.\n\n"
 
-        f"* The correlation analysis shows that the sizes of the ground floor "
-        f"living area, the first floor, the basement and the garage, play a "
-        f"key role in determining house price.\n"
-        f"* In addition, the year the house was built and the overall quality "
-        f"of materials used and the finishes in the house also play a "
-        f"significant role in determining house price.\n"
-        f"The insight will be taken to the survey team for further "
-        f"discussions and investigations."
+        f"* Some house attributes will affect the price more than others "
+        f"Size of the ground floor, living area, the basement and garage "
+        f"affect and help determine prices, by analysing historic\n"
+        f" information from the dataset we will find a guide to fit "
+        f"a price range for our customer's properties."
     )
